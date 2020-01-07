33d14ea9d3dd7e81ae1c1fda6c204b86a34e4b95
Letha0 <anna.maj94@gmail.com>
2020-01-07 02:47:25 +0100

    Added tests

---
+++ database/factories/GalleryFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Gallery;
+use Faker\Generator as Faker;
+
+
+
+$factory->define(Gallery::class, function (Faker $faker) {
+    $filepath = storage_path('images');
+
+    if(!File::exists($filepath)){
+        File::makeDirectory($filepath);  //follow the declaration to see the complete signature
+    }
+    return [
+        'author_id' => rand(1,50), 
+        'description' => $faker->sentence,
+        'photo' => $faker->image($filepath,640,480, null, false),
+    ];
+});

9eb5cd5e8b554ecbda7c6a2df84995612cff05a9
Letha0 <anna.maj94@gmail.com>
2020-01-13 21:35:34 +0100

    Compare to branch 'laravel'

--- app/Gallery.php
+++ app/Gallery.php
@@ -1,19 +1,14 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Gallery extends Model
 {
     protected $table = [
         'gallery',
     ];
-
     protected $fillable = [
         'author_id', 'filename','description','mime','original_filename',
     ];
-
     
     protected $dates = ['created_at','updated_at'];
     
@@ -21,12 +16,4 @@ class Gallery extends Model
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
-
-    protected $table ="gallery";
-
-    
-    // added because of error: Illuminate\Database\QueryException: SQLSTATE[42S02]: Base table or view not found: 1146 Table 'family-hub.galleries' doesn't exist 
-    
 }
-
-
--- app/Pivot.php
+++ app/Pivot.php
@@ -1,19 +1,14 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
 class Pivot extends Model
 {
     protected $table = [
         'pivot',
     ];
-
     protected $fillable = [
         'user_id',
     ];
-
     
     protected $dates = ['created_at','updated_at'];
     
@@ -21,8 +16,4 @@ class Pivot extends Model
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
-
-    protected $table ="pivot";
-
-    //added because of error Illuminate\Database\QueryException: SQLSTATE[42S02]: Base table or view not found: 1146 Table 'family-hub.pivots' doesn't exist
 }
